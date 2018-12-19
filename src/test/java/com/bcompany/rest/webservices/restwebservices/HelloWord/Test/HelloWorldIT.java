5f0217bce67feb328347b1cb65ea538e28732ae7
Bryan Rubia <bryanurubia@gmail.com>
2018-12-19 07:12:42 -0700

    added test

---
+++ src/test/java/com/bcompany/rest/webservices/restwebservices/HelloWord/Test/HelloWorldIT.java
@@ -0,0 +1,14 @@
+package com.bcompany.rest.webservices.restwebservices.HelloWord.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+import org.junit.Test;
+
+public class HelloWorldIT {
+
+	@Test
+	public void helloWorldTest() {
+		
+	}
+}
